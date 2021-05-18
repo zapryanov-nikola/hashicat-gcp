3815dc47817afec7155aeaad53643a46951b55cb
nikola zapryanov <nzapryanov96@gmail.com>
2021-05-18 10:33:12 +0000

    add vpc module

---
+++ vpc.tf
@@ -0,0 +1,15 @@
+module "network" {
+  source  = "app.terraform.io/nikola-training/network/google"
+  version = "2.5.0"
+  # insert required variables here
+  network_name = "guarav-network"
+  project_id = var.project
+  subnets = [
+  {
+    subnet_name   = "gaurav-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
+}
+
